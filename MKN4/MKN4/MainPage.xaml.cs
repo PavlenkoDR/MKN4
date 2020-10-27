46cd6ab1cc4d36b370cfe1660ba45ca3126e6180
pepelnica <44823082+pepelnica@users.noreply.github.com>
2020-10-27 18:49:51 +1000

    1

---
+++ MKN4/MKN4/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MKN4
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Stuff> stuffs = new ObservableCollection<Stuff>();
+
+        public SortedDictionary<string, Stuff> stuffs_dict = new SortedDictionary<string, Stuff>(); 
+        public MainPage()
+        {
+            InitializeComponent();
+            MyList.ItemsSource = stuffs;
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var page = new Page1();
+            page.Disappearing += (a, b) => {
+                Console.WriteLine("qwqw");
+
+                stuffs_dict[page.stuff.Name] =  page.stuff;
+                stuffs.Clear();
+                var temp = stuffs_dict.Select(x => { return x.Value; }).ToList();
+                foreach(var item in temp)
+                {
+                    stuffs.Add(item);
+                }
+            };
+            Navigation.PushAsync(page);
+        }
+    }
+
+    public class Stuff
+    {
+        public string Name { get; set; }
+        public string Count { get; set; }
+
+    }
+}
---
+++ MKN4/MKN4/Page1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MKN4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Stuff stuff = new Stuff();
+        public Page1()
+        {
+            InitializeComponent();
+            stuff = new Stuff() {Name="qwe", Count="3"};
+        }
+    }
+}
